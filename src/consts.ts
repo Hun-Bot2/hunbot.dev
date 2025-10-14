55cf6c5916265606bb4c2f197e011d7c81ddc3b1
Hun-Bot2 <surtrcode@gmail.com>
2025-10-14 10:57:37 +0900

    Fix error and remove useless Content

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Hun-Bot';
-export const SITE_DESCRIPTION = '개인 블로그';
+export const SITE_DESCRIPTION = 'AI/HCI 개발자 Hun의 기술 블로그';
 
 export const AUTHOR = 'Hun-Bot';
 export const AUTHOR_EMAIL = 'surtrcode@gmail.com';
