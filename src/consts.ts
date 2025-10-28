10bb29194f598e508758e273de43b57b172bbc01
Hun-Bot2 <surtrcode@gmail.com>
2025-10-28 18:04:22 +0900

    Fixing title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Hun-Bot's Devlog | AI, 개발, 그리고 기록";
+export const SITE_TITLE = "Hun-Bot's Devlog";
 export const SITE_DESCRIPTION = '개발자 Hun-Bot의 기술 블로그입니다. AI, 소프트웨어 개발, 데이터 분석, 그리고 성장에 대한 인사이트를 기록하고 공유합니다.';
 
 export const AUTHOR = 'Hun-Bot';
